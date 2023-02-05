fc4ec26f8f1d49ccb1596f21f6652674747f9de2
Yukai Huang <yukaihuangtw@gmail.com>
2023-02-05 16:47:45 +0800

    feat: fmt code

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,5 @@
-import type { JestConfigWithTsJest } from "ts-jest";
-
 import nextJest from "next/jest";
+import type { JestConfigWithTsJest } from "ts-jest";
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
