7a98fbf49d579c5f0bb2905995dc565b89fa0112
Yukai Huang <yukaihuangtw@gmail.com>
2023-02-07 21:05:00 +0800

    feat: simplify api endpoint generation

---
+++ lib/__tests__/config.spec.ts
@@ -0,0 +1,29 @@
+import { appendRentalAPIParams } from "@/lib/config";
+
+test("Should insert additional query params", () => {
+  const search = "";
+
+  const result = appendRentalAPIParams(search);
+
+  const params = new URLSearchParams(result);
+
+  expect(params.get("is_format_data")).toBe("1");
+  expect(params.get("is_new_list")).toBe("1");
+  expect(params.get("type")).toBe("1");
+
+  expect(result).toBe("is_format_data=1&is_new_list=1&type=1");
+});
+
+test("Should append additional query params", () => {
+  const search = "my_param=1";
+
+  const result = appendRentalAPIParams(search);
+
+  const params = new URLSearchParams(result);
+
+  expect(params.get("is_format_data")).toBe("1");
+  expect(params.get("is_new_list")).toBe("1");
+  expect(params.get("type")).toBe("1");
+
+  expect(result).toBe("my_param=1&is_format_data=1&is_new_list=1&type=1");
+});
