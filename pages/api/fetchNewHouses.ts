5c57ea7cd54c53d6a2a66dfe28d11dc8785704d8
Yukai Huang <yukaihuangtw@gmail.com>
2023-02-08 11:59:31 +0800

    feat: add new houses count debug

--- pages/api/fetchNewHouses.ts
+++ pages/api/fetchNewHouses.ts
@@ -30,6 +30,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const houses = await getHouseList();
     const newHouses = await store.refreshWithHouses(houses);
 
+    console.debug(`Found ${newHouses.length} new houses`);
+
     if (newHouses.length > 0) {
       await notifyTargets(newHouses);
     }
