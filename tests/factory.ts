a548564e75ca39218b8591f812293bdd64e0d123
Murmurline <steward379@gmail.com>
2023-02-13 21:40:29 +0800

    fix test

--- tests/factory.ts
+++ tests/factory.ts
@@ -3,24 +3,21 @@ import { faker } from "@faker-js/faker";
 import { DataItem } from "@/lib/types";
 
 export function houseFactory(props: Partial<DataItem> = {}): DataItem {
-  return {
+  const house: Partial<DataItem> = {
     post_id: Math.floor(Math.random() * 100000),
     title: faker.lorem.sentence(),
     type: faker.random.words(),
     price: faker.finance.amount(),
-    unit: "元/月",
-    layout: faker.random.words(),
+    price_unit: "元/月",
     area: faker.finance.amount(),
-    floorStr: faker.random.words(),
-    cases_name: faker.random.words(),
-    fulladdress: faker.address.streetAddress(),
-    rentTag: [{ id: 1, name: faker.random.words() }],
-    cover: faker.image.imageUrl(),
-    photo_alt: faker.lorem.sentence(),
-    posttime: faker.date.past(),
-    updatetime: Date.now().valueOf() / 1000,
-    browsenum_all: Math.floor(Math.random() * 100000),
+    floor_str: faker.random.words(),
+    rent_tag: [{ id: "1", name: faker.random.words() }],
+    yesterday_hit: Math.floor(Math.random() * 100),
+    refresh_time: faker.date.past().toString(),
     location: faker.address.streetAddress(),
+    photo_list: [faker.image.imageUrl()],
     ...props,
-  } as DataItem;
+  };
+
+  return house as DataItem;
 }
