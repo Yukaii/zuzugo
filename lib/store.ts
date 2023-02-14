f175b1d8681ab79b3cb6cd73f01ed085dce52aad
Yukai Huang <yukaihuangtw@gmail.com>
2023-02-14 23:46:17 +0800

    refactor: generic house store

--- lib/store.ts
+++ lib/store.ts
@@ -1,44 +1,61 @@
 import { Db, MongoClient } from "mongodb";
 
-import { DataItem } from "./types";
-
 const MAX_LIST_LENGTH = 100;
 
-export class HouseStore {
+type StoreOptions = {
+  maxListLength?: number;
+  collectionName?: string;
+  indexName?: string;
+};
+
+export class HouseStore<T, K extends keyof T> {
   private maxListLength: number;
   private client: MongoClient;
   private db: Db;
+  private collectionName: string;
+  private indexName: K;
 
-  constructor({ maxListLength }: { maxListLength?: number } = {}) {
+  constructor(
+    { maxListLength, collectionName, indexName }: StoreOptions = {
+      collectionName: "houses",
+      indexName: "post_id",
+    }
+  ) {
     this.client = new MongoClient(process.env.MONGODB_URI!);
     this.db = this.client.db();
     this.maxListLength = maxListLength || MAX_LIST_LENGTH;
+    this.collectionName = collectionName || "houses";
+    this.indexName = (indexName || "post_id") as K;
   }
 
   // Insert the new houses ids into list if they are not in the list
   // Store the house data in hash
   // Maintaining up to maxListLength houses data
-  async refreshWithHouses(houses: DataItem[]): Promise<DataItem[]> {
+  async refreshWithHouses(houses: T[]): Promise<T[]> {
     if (!houses.length) {
       return [];
     }
 
-    const houseIds = houses.map((house) => house.post_id);
+    const houseIds = houses.map((house) => house[this.indexName]);
 
-    const collection = this.db.collection("houses");
+    const collection = this.db.collection(this.collectionName);
 
     const session = this.client.startSession();
 
-    let housesToInsert: DataItem[] = [];
+    let housesToInsert: T[] = [];
     const now = Date.now();
 
     try {
       await session.withTransaction(async () => {
-        const housesInStore = await collection.find({ post_id: { $in: houseIds } }).toArray();
+        const housesInStore = await collection
+          .find({ [this.indexName as string]: { $in: houseIds } })
+          .toArray();
 
-        const housesInStoreIds = housesInStore.map((house) => house.post_id);
+        const housesInStoreIds = housesInStore.map((house) => house[this.indexName as string]);
 
-        housesToInsert = houses.filter((house) => !housesInStoreIds.includes(house.post_id));
+        housesToInsert = houses.filter(
+          (house) => !housesInStoreIds.includes(house[this.indexName])
+        );
 
         if (housesToInsert.length) {
           await collection.insertMany(
@@ -54,10 +71,12 @@ export class HouseStore {
           .skip(this.maxListLength)
           .toArray();
 
-        const housesToBeRemovedIds = housesToBeRemoved.map((house) => house.post_id);
+        const housesToBeRemovedIds = housesToBeRemoved.map(
+          (house) => house[this.indexName as string]
+        );
 
         if (housesToBeRemovedIds.length) {
-          await collection.deleteMany({ post_id: { $in: housesToBeRemovedIds } });
+          await collection.deleteMany({ [this.indexName]: { $in: housesToBeRemovedIds } });
         }
       });
     } catch (error) {
@@ -69,16 +88,16 @@ export class HouseStore {
     return housesToInsert;
   }
 
-  async getHouses(): Promise<DataItem[]> {
-    const collection = this.db.collection("houses");
+  async getHouses(): Promise<T[]> {
+    const collection = this.db.collection(this.collectionName);
 
     const houses = await collection.find({}).toArray();
 
-    return houses as unknown as DataItem[];
+    return houses as T[];
   }
 
   async clearStore() {
-    const collection = this.db.collection("houses");
+    const collection = this.db.collection(this.collectionName);
 
     await collection.deleteMany();
   }
