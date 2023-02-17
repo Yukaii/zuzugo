df8fad740e427ee6950bf78bb1110c78fee919e5
Murmurline <steward379@gmail.com>
2023-02-17 15:49:45 +0800

    Make line text message clear with partial-supported markdown syntax

--- lib/notification/line.ts
+++ lib/notification/line.ts
@@ -12,20 +12,20 @@ const generateHouseMessage = (house: DataItem) => {
   const coverPhoto = getCoverPhoto(house);
 
   return `
-新房源來了！ 🏠 \`${house.title || ""}\`
+新房源來了！ 🏠 **\`${house.title || ""}\`**
 
-💰 \`${house.price}\` ${house.price_unit}
-類型：${house.kind_name} 📐 ${house.area} 坪 🦶 在 ${house.floor_str}
+💰 ***\`${house.price}\`*** ${house.price_unit}
+*類型*：${house.kind_name} 📐 **${house.area}** 坪 🦶 在 ${house.floor_str}
 ${house.community} 🗺️ ${house.location}
 
-📌Google Maps：https://www.google.com/maps/search/?api=1&query=${house.location}
+📌*Google Map* https://www.google.com/maps/search/?api=1&query=${house.location}
 
-👉 👉 👉 ${house.refresh_time}更新
+*${house.refresh_time}更新*
 
-🌍打開網站：https://rent.591.com.tw/home/${house.post_id}
-📱手機 App：${mobileUrl}
-\`${house.rent_tag?.map((tag) => tag.name)}\`
-👀 昨天有 ${house.yesterday_hit} 人瀏覽
+🌍==打開網站==：https://rent.591.com.tw/home/${house.post_id}
+📱==手機 App==：${mobileUrl}
+**\`${house.rent_tag?.map((tag) => tag.name)}\`**
+*👀 昨天有 ${house.yesterday_hit} 人瀏覽*
 `;
 };
 
