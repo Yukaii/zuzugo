e266d10f0014463518346cecee21dbff7ba5efa5
Murmurline <steward379@gmail.com>
2023-02-17 16:34:07 +0800

    Allow the photo msg to be none (`undefined`) if there isn't any (#33)
    - Allow the photo msg to be none (`undefined`) if there isn't any
    - Optimize the line text msg format
    - Enable to send message without image
    - Add a conditional expressions to handle situations with or without image msg.
    - Delete all the line token parameter in core logic
    - Brief line object property with Es6 syntax sugar from `a:a` to `a`
    - Make line text message clear with partial-supported markdown syntax

--- lib/sendLineNotify.ts
+++ lib/sendLineNotify.ts
@@ -1,50 +1,34 @@
 import { got } from "got";
 
-export async function sendLineNotify(
-  messageContent: string,
-  msgImg: string,
-  lineNotifyToken: string | undefined
-) {
+import { config } from "./config";
+
+export async function sendLineNotify(message: string, msgImg?: string) {
   console.log("sendLineNotify");
 
   try {
+    const form = msgImg
+      ? {
+          imageThumbnail: msgImg,
+          imageFullsize: msgImg,
+          message,
+        }
+      : {
+          message,
+        };
     const resp = await got.post({
       url: "https://notify-api.line.me/api/notify",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: `Bearer ${lineNotifyToken}`,
+        Authorization: `Bearer ${config.tokenLine}`,
       },
-      form: {
-        imageThumbnail: msgImg,
-        imageFullsize: msgImg,
-        message: messageContent,
-      },
+      form,
     });
 
-    // const resp = await got.post({
-    //   url: "https://notify-api.line.me/api/notify",
-    //   headers: {
-    //     "Content-Type": "application/x-www-form-urlencoded",
-    //     Authorization: `Bearer ${lineNotifyToken}`,
-    //   },
-    //   body: new URLSearchParams({
-    //     imageThumbnail: msgImg,
-    //     imageFullsize: msgImg,
-    //     // message: messageContent,
-    //   }).toString(),
-    //   // form: {
-    //   //   message: messageContent,
-    //   //   // stickerPackageId: 6136,
-    //   //   // stickerId: 10551386,
-    //   //   imageThumbnail: msgImg,
-    //   //   imageFullsize: msgImg,
-    //   // },
-    // });
     return resp;
   } catch (error) {
     console.error(error as any);
     console.error((error as any)?.response?.body);
 
-    throw `Line Token 可能過期了`;
+    throw `Line Token 可能過期了或 Message API 有問題`;
   }
 }
