5befcc365f161e034a75bef9fa0f1739058ded0f
Yukai Huang <yukaihuangtw@gmail.com>
2023-02-23 23:09:02 +0800

    New notification system with inngest (#45)
    * feat: install inngest packages
    
    * refactor: send notification with inngest
    
    * test: increase getToken test timeout to 10s

--- lib/__tests__/getToken.spec.ts
+++ lib/__tests__/getToken.spec.ts
@@ -4,7 +4,7 @@ test("getToken should return string results", async function () {
   const csrfToken = await getToken();
 
   expect(typeof csrfToken).toBe("string");
-});
+}, 10000);
 
 test("getHouseList should return array of house data", async function () {
   const houseList = await getHouseList();
---
+++ lib/inngest/index.ts
@@ -0,0 +1,3 @@
+import { dispatchAll, notifyLineNotification, notifySlackNotification } from "@/lib/notification";
+
+export const fns = [dispatchAll, notifyLineNotification, notifySlackNotification];
