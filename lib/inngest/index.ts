88ec7d8d466a3d7c053f37d5a19e35714068d880
Yukai Huang <yukaihuangtw@gmail.com>
2023-02-25 15:25:58 +0800

    feat: new inngest cron job to crawl new houses to database (#66)

--- lib/inngest/index.ts
+++ lib/inngest/index.ts
@@ -1,3 +1,4 @@
 import { dispatchAll, notifyLineNotification, notifySlackNotification } from "@/lib/notification";
+import { fetchNewHousesFn } from "@/lib/tasks";
 
-export const fns = [dispatchAll, notifyLineNotification, notifySlackNotification];
+export const fns = [dispatchAll, notifyLineNotification, notifySlackNotification, fetchNewHousesFn];
