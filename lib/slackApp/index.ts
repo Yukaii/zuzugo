4ec806e90e34e40b129d4576499df23376ba7a95
Yukai Huang <yukaihuangtw@gmail.com>
2023-02-27 22:42:21 +0800

    feat: slack dev mode (#82)
    resolve #11

---
+++ lib/slackApp/index.ts
@@ -0,0 +1,51 @@
+import { AppRunner, AppRunnerOptions } from "@seratch_/bolt-http-runner";
+import { App, AppOptions, LogLevel } from "@slack/bolt";
+
+import { config } from "@/lib/config";
+
+export let appRunner: AppRunner | undefined;
+
+export function setupSlackApp(setupApp: (app: App) => void) {
+  const isDevMode = config.enableSocketModeForDev;
+
+  if (isDevMode) {
+    console.log(
+      "⚡️ Bolt app is running with Socket Mode, remember to set up the socket mode in the slack app settings!"
+    );
+  }
+
+  const baseAppOptions: AppOptions = {
+    logLevel: LogLevel.DEBUG,
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET!,
+    scopes: ["commands", "chat:write"],
+  };
+
+  // before start
+  if (!isDevMode) {
+    appRunner = new AppRunner(baseAppOptions as AppRunnerOptions);
+  }
+
+  let appOptions;
+  if (isDevMode) {
+    appOptions = {
+      ...baseAppOptions,
+      socketMode: true,
+      appToken: config.slackAppToken,
+    };
+  } else {
+    appOptions = { ...appRunner!.appOptions() };
+  }
+
+  const app = new App(appOptions);
+
+  setupApp(app);
+
+  if (isDevMode) {
+    app.start().then(() => {
+      console.log("⚡️ Bolt app is running with Socket Mode!");
+    });
+  } else {
+    appRunner!.setup(app);
+  }
+}
