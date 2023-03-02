ba8e85798db6677ff1041851fe33a45520729376
Alan Chou <alan10332000@gmail.com>
2023-03-02 20:05:12 +0800

    feat: add msw handlers

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,9 +1,19 @@
 import { rest } from "msw";
 
+import { config } from "@/lib/config";
 import rsList from "@/tests/fixtures/rsList.json";
+
+const houseListURL = config.houseListURL;
 
 export const handlers = [
   rest.get("https://rent.591.com.tw", (req, res, ctx) => {
-    return res(ctx.json(rsList));
+    return res(
+      ctx.status(200),
+      ctx.text('<meta name="csrf-token" content="uC9EMNpcOACJVtvcPq090jzDIVv4bzJex4TmXGEd" />')
+    );
+  }),
+
+  rest.get(houseListURL, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(rsList));
   }),
 ];
