fb349c54fd5e8441707c27285e95aa2ee83e7f8a
Yukai Huang <yukaihuangtw@gmail.com>
2023-03-19 18:40:06 +0800

    feat: help message

--- lib/slackApp/commands.ts
+++ lib/slackApp/commands.ts
@@ -6,6 +6,7 @@ import { config } from "@/lib/config";
 import { inngest } from "@/lib/inngest/client";
 import { prisma } from "@/lib/prisma";
 import { Subscriptions } from "@/lib/slackApp/components/Subscriptions";
+import { helpMessage } from "@/lib/slackApp/helpCommand";
 
 type CommandHandler = (args: {
   webhook: IncomingWebhook;
@@ -149,40 +150,7 @@ const commandHandlers: Record<string, CommandHandler> = {
     }
   },
   help: async ({ webhook }) => {
-    await webhook.send({
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: "Hi there :wave: here are some ideas of what you can do:",
-          },
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: "*Subscribe to queries*\nSubscribe\n`/zuzugo subscribe query`\nShow subscriptions\n`/zuzugo subscribe list`\nUnsubscribe\n`/zuzugo unsubscribe subscriptionId`",
-          },
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: "For more information, visit our documentation:",
-          },
-          accessory: {
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Documentation",
-              emoji: true,
-            },
-            url: "https://example.com/docs",
-          },
-        },
-      ],
-    });
+    await webhook.send(helpMessage);
   },
 };
 
---
+++ lib/slackApp/helpCommand.ts
@@ -0,0 +1,34 @@
+export const helpMessage = {
+  blocks: [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: "Hi there :wave: here are some ideas of what you can do:",
+      },
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: "*Subscribe to queries*\nSubscribe\n`/zuzugo subscribe query`\nShow subscriptions\n`/zuzugo subscribe list`\nUnsubscribe\n`/zuzugo unsubscribe subscriptionId`",
+      },
+    },
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: "For more information, visit our documentation:",
+      },
+      accessory: {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "Documentation",
+          emoji: true,
+        },
+        url: "https://example.com/docs",
+      },
+    },
+  ],
+};
